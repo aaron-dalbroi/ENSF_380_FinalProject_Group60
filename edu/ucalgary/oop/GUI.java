e8986e0961f8e3693df1bb187cbf67ce901049cd
Romanard Tiratira <romanard.tiratira@ucalgary.ca>
2023-03-30 10:17:24 -0600

    Updated GUI mock up to take in inputs

--- edu/ucalgary/oop/GUI.java
+++ edu/ucalgary/oop/GUI.java
@@ -26,17 +26,21 @@ public class GUI implements ActionListener{
     }
         
     public void actionPerformed(ActionEvent event){
-            Boolean error = true;
-            //input = 0 if "ok" is pressed
-            int input;
-            if(error){
-                input = JOptionPane.showConfirmDialog(null, createShedule(), "Error", JOptionPane.DEFAULT_OPTION);
+            String result = createShedule();
+            String output;
+            if(result == "Error has occured"){
+                String[] options = {"option 1", "option 2", "option 3"};
+                output = (String) JOptionPane.showInputDialog(null, "Error has occured", "Error", JOptionPane.ERROR_MESSAGE, null, options, options[0]);
+                if(output != null){
+                    String[] newOptions = {"option 4", "option 5", "option 6"};
+                    output = (String) JOptionPane.showInputDialog(null, "Phase 2", "Phase 2", JOptionPane.ERROR_MESSAGE, null, newOptions, options[0]);
+                }
             }else{
-                input = JOptionPane.showConfirmDialog(null, createShedule(), "Good", JOptionPane.DEFAULT_OPTION);
+                JOptionPane.showConfirmDialog(null, createShedule(), "Good", JOptionPane.DEFAULT_OPTION);
             }
         }
 
     private String createShedule() {
-        return "error has occured";
+        return "Error has occured";
     }
 }
