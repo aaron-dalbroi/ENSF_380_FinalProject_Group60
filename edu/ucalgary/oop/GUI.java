2ebf3fced492e8ccf40374047722d06fbe8f73b2
r-tiratira <113866883+r-tiratira@users.noreply.github.com>
2023-04-02 14:52:53 -0600

    Update GUI.java

--- edu/ucalgary/oop/GUI.java
+++ edu/ucalgary/oop/GUI.java
@@ -15,7 +15,6 @@ public class GUI implements ActionListener{
                 e.printStackTrace();
             }
             String result = createShedule();
-            String output;
             String task;
             String time;
             if(result == "Schedule need to be updated"){
@@ -26,7 +25,7 @@ public class GUI implements ActionListener{
                 }
                 Object[] options = temp.toArray();
                 task = (String) JOptionPane.showInputDialog(null, "Schedule is not possible", "Impossible schedule", JOptionPane.ERROR_MESSAGE, null, options, options[0]);
-                if(output != null){
+                if(task != null){
                     String[] newOptions = {"0","1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23"};
                     time = (String) JOptionPane.showInputDialog(null, "Pick the time to change it to", "Choose time", JOptionPane.ERROR_MESSAGE, null, newOptions, options[0]);
                 }
