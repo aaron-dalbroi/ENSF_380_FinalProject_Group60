e0a8bffa973e66e99e131209f2fa0782cd372ec3
r-tiratira <113866883+r-tiratira@users.noreply.github.com>
2023-04-02 13:31:19 -0600

    Further updated GUI
    The gui can handle the multiple types of errors.
    Further updated the option given to the user.

--- edu/ucalgary/oop/GUI.java
+++ edu/ucalgary/oop/GUI.java
@@ -3,24 +3,39 @@ package edu.ucalgary.oop;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class GUI implements ActionListener{
     public void actionPerformed(ActionEvent event){
+            SqlConnection connection = null;
+            try {
+                connection = new SqlConnection();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
             String result = createShedule();
             String output;
-            if(result == "Error has occured"){
-                String[] options = {"option 1", "option 2", "option 3"};
-                output = (String) JOptionPane.showInputDialog(null, "Error has occured", "Error", JOptionPane.ERROR_MESSAGE, null, options, options[0]);
+            if(result == "Schedule need to be updated"){
+                ArrayList<Entry> entries = connection.pullTreatmentEntries();
+                ArrayList<String> temp = new ArrayList<>();
+                for(Entry entry : entries){
+                    temp.add(entry.getTask() + " for " + entry.getName() + " at " + entry.getStartTime());
+                }
+                Object[] options = temp.toArray();
+                output = (String) JOptionPane.showInputDialog(null, "Schedule is not possible", "Impossible schedule", JOptionPane.ERROR_MESSAGE, null, options, options[0]);
                 if(output != null){
-                    String[] newOptions = {"option 4", "option 5", "option 6"};
+                    String[] newOptions = {"0","1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23"};
                     output = (String) JOptionPane.showInputDialog(null, "Phase 2", "Phase 2", JOptionPane.ERROR_MESSAGE, null, newOptions, options[0]);
                 }
+            }else if(result == "Volunteer needs to be called"){
+                int input = JOptionPane.showConfirmDialog(null, "A volunteer needs to added", "Volunteer needed", JOptionPane.DEFAULT_OPTION);
             }else{
-                JOptionPane.showConfirmDialog(null, createShedule(), "Good", JOptionPane.DEFAULT_OPTION);
+                int input = JOptionPane.showConfirmDialog(null, "Schedule", "good", JOptionPane.DEFAULT_OPTION);
             }
         }
 
     private String createShedule() {
-        return "Error has occured";
+        return "Schedule need to be updated";
     }
 }
