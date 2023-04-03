b6a8eaafd24640fc652fdcdef26bd250fb6f9924
Zach Pelkey <113135691+zpelkey03@users.noreply.github.com>
2023-04-03 16:51:02 -0600

    Update HourTest.java

--- edu/ucalgary/oop/HourTest.java
+++ edu/ucalgary/oop/HourTest.java
@@ -1,35 +1,34 @@
-//package edu.ucalgary.oop;
-//
-//import org.junit.Test;
-//import static org.junit.Assert.*;
-//
-//public class HourTest {
-//
-//    @Test
-//    public void testGetters(){
-//        Hour testObject = new Hour(1);
-//        testObject.addTimeSpent(40);
-//        int correctStart = 1;
-//        int timeSpent = 40;
-//        int timeAvailable = 20;
-//        boolean volunteerNeeded = false;
-//
-//        assertEquals("Did not output correct hour" ,correctStart, testObject.getTime());
-//        assertEquals("Did not output correct time spent",timeSpent, testObject.getTimeSpent());
-//        assertEquals("Did not output correct time available",timeAvailable, testObject.getTimeAvailable());
-//        assertEquals("Did not output correct id",volunteerNeeded, testObject.isVolunteerNeeded());
-//    }
-//
-//    @Test
-//    public void testTimeException(){
-//
-//        try{
-//            Hour testObject = new Hour(-1);
-//            fail("Did not throw IllegalArgumentException");
-//        }catch(IllegalArgumentException e){
-//            return;
-//        }
-//    }
-//
-//
-//}
+    package edu.ucalgary.oop;
+
+    import org.junit.Test;
+    import static org.junit.Assert.*;
+
+    public class HourTest {
+
+        @Test
+        public void testGetters(){
+            Hour testObject = new Hour(1);
+            testObject.subtractTimeAvailable(20);
+            Entry correctEntry = new Entry( "task", 4, 4, 20, 4, "coyote", "Eddie");
+            testObject.addTaskToHour(correctEntry);
+            int correctStart = 1;
+            int timeAvailable = 40;
+            boolean volunteerNeeded = false;
+            assertEquals("Did not output correct hour" ,correctStart, testObject.getTime());
+            assertEquals("Did not output correct time available",timeAvailable, testObject.getTimeAvailable());
+            assertEquals("Did not output correct Entry", correctEntry, testObject.getTasks().get(0));
+        }
+
+        @Test
+        public void testTimeException(){
+
+            try{
+                Hour testObject = new Hour(-1);
+                fail("Did not throw IllegalArgumentException");
+            }catch(IllegalArgumentException e){
+                return;
+            }
+        }
+
+
+    }
