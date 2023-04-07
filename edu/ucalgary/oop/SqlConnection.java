d06394a92edb7e126e8ba815366b6fd3d71371c4
Mario Rodriguez <113868852+MarioR03@users.noreply.github.com>
2023-04-07 16:46:27 -0600

    created a new Main class that prints off all newly created tasks (#28)

--- edu/ucalgary/oop/SqlConnection.java
+++ edu/ucalgary/oop/SqlConnection.java
@@ -37,7 +37,7 @@ public class SqlConnection {
 
         try{
             //the connection info here will need to be changed depending on the user
-            dbConnect = DriverManager.getConnection("jdbc:mysql://localhost/ewr", "root", "AbXy219!");
+            dbConnect = DriverManager.getConnection("jdbc:mysql://localhost/ewr", "root", "Lulumybaby1.");
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -69,7 +69,6 @@ public class SqlConnection {
             int id = results.getInt("AnimalID");
             String name = results.getString("AnimalNickName");
             String species = results.getString("AnimalSpecies");
-            System.out.println(name);
             if(species.equals("coyote")){
                 listOfAnimals.add(new Coyote(name,id));
             }
@@ -88,26 +87,6 @@ public class SqlConnection {
         }
 
         return listOfAnimals;
-    }
-
-    public void print(){
-
-        try {
-            Statement myStmt = dbConnect.createStatement();
-            myStmt.executeQuery("USE EWR");
-            results = myStmt.executeQuery("SELECT * FROM ANIMALS");
-
-            while(results.next()){
-                String result = results.getString(0);
-                System.out.println(result);
-
-            }
-
-        }
-        catch(Exception e){
-            System.out.println(e);
-        }
-
     }
 
     /**
@@ -153,100 +132,100 @@ public class SqlConnection {
         return arrTaskInfo;
     }
 
-    /**
-     * pullCleaningEntries
-     *
-     * Looks at every animal in the database and generates an appropriate cleaning Entry
-     * @return An array of all cleaning tasks generated
-     *
-     */
-    public ArrayList<ArrayList<String>> pullCleaningTasks(){
-        ArrayList<ArrayList<String>> arrTaskInfo = new ArrayList<>();
-        try {
-            Statement myStmt = dbConnect.createStatement();
-            myStmt.executeQuery("USE EWR");
-            results = myStmt.executeQuery("SELECT * FROM ANIMALS");
-
-            while(results.next()){
-                String task = "Cage cleaning";
-                String animalType = results.getString("AnimalSpecies");
-                int startTime = 0;
-                int maxWindow = 24;
-
-                int animalID = results.getInt("animalID");
-                String animalName = results.getString("AnimalNickname");
-                String animalEnum = results.getString("AnimalSpecies").toUpperCase();
-                int duration = AnimalSpecies.valueOf(animalEnum).getCleaningTime();
-
-
-
-                Entry newEntry = new Entry(task,startTime,maxWindow,duration,animalID, animalType,animalName);
-                listOfEntries.add(newEntry);
-            }
-        }
-        catch(Exception e){
-            System.out.println(e);
-        }
-        return listOfEntries;
-    }
-    /**
-     * pullFeedingEntries
-     *
-     * Looks at every animal in the database and generates an appropriate feeding Entry
-     * @return An array of all feeding tasks generated
-     *
-     */
-    public ArrayList<Entry> pullFeedingEntries() throws IllegalArgumentException{
-        ArrayList<Entry> listOfEntries = new ArrayList<>();
-        try {
-            Statement myStmt = dbConnect.createStatement();
-            myStmt.executeQuery("USE EWR");
-            results = myStmt.executeQuery("SELECT * FROM ANIMALS");
-
-            while(results.next()){
-                String task = results.getString("AnimalSpecies") + " Feeding";
-                int animalID = results.getInt("animalID");
-                String animalType = results.getString("AnimalSpecies");
-                String animalName = results.getString("AnimalNickname");
-
-                //getting the startTime, maxWindow and duration from the animal Enum
-                String animalEnum = animalType.toUpperCase();
-                // These if statements are used to get the value of startTime based on the chronotype
-                String chronoType = AnimalSpecies.valueOf(animalEnum).getChronoType();
-                int startTime;
-                if (chronoType.equals("nocturnal")){
-                    startTime = 0;
-                } else if (chronoType.equals("crepuscular")) {
-                    startTime = 19;
-                } else if (chronoType.equals("diurnal")) {
-                    startTime = 8;
-                } else {
-                    throw new IllegalArgumentException("Chronotype not found");
-                }
-                //The max feeding window for any chronotype is always 3 hours.
-                int maxWindow = 3;
-                int duration = AnimalSpecies.valueOf(animalEnum).getFeedingDuration();
-
-                Entry newEntry = new Entry(task,startTime,maxWindow,duration,animalID, animalType,animalName);
-                listOfEntries.add(newEntry);
-            }
-        }
-        catch(Exception e){
-            System.out.println(e);
-        }
-        return listOfEntries;
-    }
-
-    public void updateTreatment(int animalID, int taskID, int newStartHour) {
-        try {
-            PreparedStatement myStmt = dbConnect.prepareStatement("UPDATE TREATMENTS SET StartHour = ? WHERE AnimalID = ? AND TaskID = ?");
-            myStmt.setInt(1, newStartHour);
-            myStmt.setInt(2, animalID);
-            myStmt.setInt(3, taskID);
-            myStmt.executeUpdate();
-        } catch (Exception e) {
-            System.out.println(e);
-        }
-    }
+//    /**
+//     * pullCleaningEntries
+//     *
+//     * Looks at every animal in the database and generates an appropriate cleaning Entry
+//     * @return An array of all cleaning tasks generated
+//     *
+//     */
+//    public ArrayList<ArrayList<String>> pullCleaningTasks(){
+//        ArrayList<ArrayList<String>> arrTaskInfo = new ArrayList<>();
+//        try {
+//            Statement myStmt = dbConnect.createStatement();
+//            myStmt.executeQuery("USE EWR");
+//            results = myStmt.executeQuery("SELECT * FROM ANIMALS");
+//
+//            while(results.next()){
+//                String task = "Cage cleaning";
+//                String animalType = results.getString("AnimalSpecies");
+//                int startTime = 0;
+//                int maxWindow = 24;
+//
+//                int animalID = results.getInt("animalID");
+//                String animalName = results.getString("AnimalNickname");
+//                String animalEnum = results.getString("AnimalSpecies").toUpperCase();
+//                int duration = AnimalSpecies.valueOf(animalEnum).getCleaningTime();
+//
+//
+//
+//                Entry newEntry = new Entry(task,startTime,maxWindow,duration,animalID, animalType,animalName);
+//                listOfEntries.add(newEntry);
+//            }
+//        }
+//        catch(Exception e){
+//            System.out.println(e);
+//        }
+//        return listOfEntries;
+//    }
+//    /**
+//     * pullFeedingEntries
+//     *
+//     * Looks at every animal in the database and generates an appropriate feeding Entry
+//     * @return An array of all feeding tasks generated
+//     *
+//     */
+//    public ArrayList<Entry> pullFeedingEntries() throws IllegalArgumentException{
+//        ArrayList<Entry> listOfEntries = new ArrayList<>();
+//        try {
+//            Statement myStmt = dbConnect.createStatement();
+//            myStmt.executeQuery("USE EWR");
+//            results = myStmt.executeQuery("SELECT * FROM ANIMALS");
+//
+//            while(results.next()){
+//                String task = results.getString("AnimalSpecies") + " Feeding";
+//                int animalID = results.getInt("animalID");
+//                String animalType = results.getString("AnimalSpecies");
+//                String animalName = results.getString("AnimalNickname");
+//
+//                //getting the startTime, maxWindow and duration from the animal Enum
+//                String animalEnum = animalType.toUpperCase();
+//                // These if statements are used to get the value of startTime based on the chronotype
+//                String chronoType = AnimalSpecies.valueOf(animalEnum).getChronoType();
+//                int startTime;
+//                if (chronoType.equals("nocturnal")){
+//                    startTime = 0;
+//                } else if (chronoType.equals("crepuscular")) {
+//                    startTime = 19;
+//                } else if (chronoType.equals("diurnal")) {
+//                    startTime = 8;
+//                } else {
+//                    throw new IllegalArgumentException("Chronotype not found");
+//                }
+//                //The max feeding window for any chronotype is always 3 hours.
+//                int maxWindow = 3;
+//                int duration = AnimalSpecies.valueOf(animalEnum).getFeedingDuration();
+//
+//                Entry newEntry = new Entry(task,startTime,maxWindow,duration,animalID, animalType,animalName);
+//                listOfEntries.add(newEntry);
+//            }
+//        }
+//        catch(Exception e){
+//            System.out.println(e);
+//        }
+//        return listOfEntries;
+//    }
+//
+//    public void updateTreatment(int animalID, int taskID, int newStartHour) {
+//        try {
+//            PreparedStatement myStmt = dbConnect.prepareStatement("UPDATE TREATMENTS SET StartHour = ? WHERE AnimalID = ? AND TaskID = ?");
+//            myStmt.setInt(1, newStartHour);
+//            myStmt.setInt(2, animalID);
+//            myStmt.setInt(3, taskID);
+//            myStmt.executeUpdate();
+//        } catch (Exception e) {
+//            System.out.println(e);
+//        }
+//    }
 
 }
