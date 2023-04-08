b835550998700ea9caa54db4d8909cb1384a623c
zpelkey03 <113135691+zpelkey03@users.noreply.github.com>
2023-04-08 15:41:00 -0600

    added some fixed tests

--- edu/ucalgary/oop/tests/HourTest.java
+++ edu/ucalgary/oop/tests/HourTest.java
@@ -1,47 +1,57 @@
-//    package edu.ucalgary.oop.tests;
-//
-//    import edu.ucalgary.oop.Entry;
-//    import edu.ucalgary.oop.Hour;
-//    import org.junit.Test;
-//    import static org.junit.Assert.*;
-//
-//    public class HourTest {
-//
-//        /**
-//         * Hour() is called with a valid hour
-//         * subtractTimeAvailable() is called with a valid time
-//         * addTaskToHour() is called with a valid task
-//         * All getters are called
-//         */
-//        @Test
-//        public void testGetters(){
-//            Hour testObject = new Hour(1);
-//            Entry correctEntry = new Entry( "task", 4, 4, 20, 4, "coyote", "Eddie");
-//            testObject.subtractTimeAvailable(correctEntry.getDuration());
-//            testObject.addTaskToHour(correctEntry);
-//            int correctStart = 1;
-//            int timeAvailable = 40;
-//            assertEquals("Did not output correct hour" ,correctStart, testObject.getTime());
-//            assertEquals("Did not output correct time available",timeAvailable, testObject.getTimeAvailable());
-//            assertEquals("Did not output correct Entry", correctEntry, testObject.getTasks().get(0));
-//        }
-//
-//        /**
-//         *
-//         * Hour() is called with an invalid-hour. IllegalArgumentException should be thrown
-//         *
-//         */
-//        @Test
-//        public void testTimeException(){
-//
-//            try{
-//                Hour testObject = new Hour(-1);
-//                Hour testObject1 = new Hour(24);
-//                fail("Did not throw IllegalArgumentException");
-//            }catch(IllegalArgumentException e){
-//                return;
-//            }
-//        }
-//
-//
-//    }
+    package edu.ucalgary.oop.tests;
+
+    import edu.ucalgary.oop.Animal;
+    import edu.ucalgary.oop.Coyote;
+    import edu.ucalgary.oop.Hour;
+    import edu.ucalgary.oop.Task;
+    import org.junit.Test;
+    import static org.junit.Assert.*;
+
+    public class HourTest {
+
+        Animal testAnimal = new Coyote("Barry", 2);
+        Task testTask = new Task(testAnimal, "Feed Barry some berries", 7, 2, 4, 20);
+
+        /**
+         * Hour() is called with a valid hour
+         * subtractTimeAvailable() is called with a valid time
+         * addTaskToHour() is called with a valid task
+         * All getters are called
+         */
+        @Test
+        public void testGetters(){
+            Hour testObject = new Hour(2);
+            testObject.addTaskToHour(testTask);
+            testObject.subtractTimeAvailable(testTask.getDuration());
+
+            int correctTime = 2;
+            int correctTimeAvailable = 40;
+
+            assertEquals("Did not output correct hour" ,correctTime, testObject.getTime());
+            assertEquals("Did not output correct time available",correctTimeAvailable, testObject.getTimeAvailable());
+            assertEquals("Did not output correct Entry", testTask, testObject.getTasks().get(0));
+        }
+
+        /**
+         *
+         * Hour() is called with an invalid-hour. IllegalArgumentException should be thrown
+         *
+         */
+        @Test
+        public void testTimeException(){
+            try{
+                Hour testObject = new Hour(-1);
+                fail("Did not throw IllegalArgumentException");
+            }catch(IllegalArgumentException e){
+                return;
+            }
+            try{
+                Hour testObject1 = new Hour(24);
+                fail("Did not throw IllegalArgumentException");
+            }catch(IllegalArgumentException e){
+                return;
+            }
+        }
+
+
+    }
