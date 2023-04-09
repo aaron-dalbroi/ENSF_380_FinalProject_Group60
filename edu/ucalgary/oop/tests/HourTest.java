48751096a9f8d722e8c5fdf9db3418711a623313
zpelkey03 <113135691+zpelkey03@users.noreply.github.com>
2023-04-09 12:25:36 -0600

    Added the hour overflow test

--- edu/ucalgary/oop/tests/HourTest.java
+++ edu/ucalgary/oop/tests/HourTest.java
@@ -11,6 +11,8 @@
 
         Animal testAnimal = new Coyote("Barry", 2);
         Task testTask = new Task(testAnimal, "Feed Barry some berries", 7, 2, 4, 20);
+        Task testTask2 = new Task(testAnimal, "Give Barry a bath", 7, 2, 4, 40);
+        Task testTask3 = new Task(testAnimal, "Tell Barry he's the best", 7, 2, 4, 10);
 
         /**
          * Hour() is called with a valid hour
@@ -30,6 +32,33 @@
             assertEquals("Did not output correct hour" ,correctTime, testObject.getTime());
             assertEquals("Did not output correct time available",correctTimeAvailable, testObject.getTimeAvailable());
             assertEquals("Did not output correct Entry", testTask, testObject.getTasks().get(0));
+        }
+
+        /*
+            * Hour() is called with a valid hour
+            * addTaskToHour() is called with a valid task
+            * Hour is overflowed over 60 minutes
+            * time available should be negative
+         */
+
+        @Test
+        public void testOverflow(){
+            Hour testObject = new Hour(2);
+            testObject.addTaskToHour(testTask);
+            testObject.addTaskToHour(testTask2);
+            testObject.addTaskToHour(testTask3);
+            testObject.subtractTimeAvailable(testTask.getDuration());
+            testObject.subtractTimeAvailable(testTask2.getDuration());
+            testObject.subtractTimeAvailable(testTask3.getDuration());
+
+            int correctTime = 2;
+            int correctTimeAvailable = -10;
+
+            assertEquals("Did not output correct hour" ,correctTime, testObject.getTime());
+            assertEquals("Did not output correct time available",correctTimeAvailable, testObject.getTimeAvailable());
+            assertEquals("Did not output correct Entry", testTask, testObject.getTasks().get(0));
+            assertEquals("Did not output correct Entry", testTask2, testObject.getTasks().get(1));
+            assertEquals("Did not output correct Entry", testTask3, testObject.getTasks().get(2));
         }
 
         /**
